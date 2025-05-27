ef8a25b73e368574bde41c772dbb2c42ce83addf
thebasedtaka <darktaka9998@gmail.com>
2025-05-26 21:42:09 -0400

    Error Handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { getCategories } from "./api/categories";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import Page from "./pages/Page";
 
 function App() {
