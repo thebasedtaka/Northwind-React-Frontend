27774890ee2abe145461a7df4b179842e7cc9e5d
thebasedtaka <darktaka9998@gmail.com>
2025-05-27 19:54:37 -0400

    Test api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("API_BASE_URL at runtime:", API_BASE_URL);
 const northwindClient = axios.create({
   baseURL: `${API_BASE_URL}/api/`,
   timeout: 10000,
